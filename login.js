bfd169bde87e4747ebceb82d54bf689c4808538a
Rafael Pessoa <rafael.pessoa.alexandre@gmail.com>
2024-09-05 18:02:51 -0300

    feat: implementação da lógica na página de login

---
+++ login.js
@@ -0,0 +1,15 @@
+function redirect() {
+    window.location.href = 'introducao_3.html';
+}
+
+function redirect_1() {
+    window.location.href = 'https://www.facebook.com';
+}
+
+function redirect_2() {
+    window.location.href = 'https://www.google.com';
+}
+
+function redirect_3() {
+    window.location.href = 'https://www.apple.com';
+}
